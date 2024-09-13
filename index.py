daa27c60c44d0dfbd2a8d4cf070aaa30cd47f29e
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2024-09-13 07:36:42 +0530

    Code completed

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 import numpy as np
 from joblib import load
 from sklearn.datasets import load_iris
-from mangum import Mangum
 
 target_names = load_iris().target_names
 
@@ -15,23 +14,25 @@ class input(BaseModel):
     features: list
 
 @app.get("/")
-async def home():
+def home():
     return {"message": "This is a home page"}
 
 @app.get("/about")
-async def test():
+def test():
     return {"hello world": "hello world"}
 
 @app.post("/predict/rf")
-async def predict_sklearn(data: input):
+def predict_sklearn(data: input):
     x_input = np.array(data.features).reshape(1, -1)
     prediction = rf_model.predict(x_input)
     return {"prediction": target_names[prediction[0]]}
 
 @app.post("/predict/knn")
-async def predict_knn(data: input):
+def predict_knn(data: input):
     x_input = np.array(data.features).reshape(1, -1)
     prediction = knn_model.predict(x_input)
     return {"prediction": target_names[prediction][0]}
 
-handler = Mangum(app) 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
