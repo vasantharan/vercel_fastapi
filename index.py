77502fdae8f6efdafed960ccfcdf61d76b51861d
KUMARANVASANTH <kumarakurubaran944@gmail.com>
2024-09-13 07:30:27 +0530

    Code completed

--- index.py
+++ index.py
@@ -8,29 +8,30 @@ from mangum import Mangum
 target_names = load_iris().target_names
 
 app = FastAPI()
-handler = Mangum(app) 
 rf_model = load('./Random_Forest.joblib')
 knn_model = load('./KNN.joblib')
 
 class input(BaseModel):
     features: list
 
-@app.get("/home")
-def home():
+@app.get("/")
+async def home():
     return {"message": "This is a home page"}
 
 @app.get("/about")
-def test():
+async def test():
     return {"hello world": "hello world"}
 
 @app.post("/predict/rf")
-def predict_sklearn(data: input):
+async def predict_sklearn(data: input):
     x_input = np.array(data.features).reshape(1, -1)
     prediction = rf_model.predict(x_input)
     return {"prediction": target_names[prediction[0]]}
 
 @app.post("/predict/knn")
-def predict_knn(data: input):
+async def predict_knn(data: input):
     x_input = np.array(data.features).reshape(1, -1)
     prediction = knn_model.predict(x_input)
     return {"prediction": target_names[prediction][0]}
+
+handler = Mangum(app) 
